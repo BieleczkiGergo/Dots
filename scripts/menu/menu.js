1f0007e3cf3dfaa46dee4ea389cbe2a95f660775
Bieleczki Gergő Péter <20d_bieleczkig@NYIRSZIKSZI.local>
2023-06-01 08:24:06 +0200

    some icons added

--- scripts/menu/menu.js
+++ scripts/menu/menu.js
@@ -6,7 +6,7 @@ class Menu{
         this.menu = document.getElementById("menu");
 
         new MenuItem(modes.grab, "./pics/noIcon.svg", "grab/move nodes", "grab/move", this.menu);
-        new MenuItem(modes.newNode, "./pics/noIcon.svg", "new node", "new node", this.menu);
+        new MenuItem(modes.newNode, "./pics/suggestions/newDot.svg", "new node", "new node", this.menu);
         new MenuItem(modes.newConnection, "./pics/noIcon.svg", "new connection", "new connection", this.menu);
 
         this.menu.onclick = function(){
